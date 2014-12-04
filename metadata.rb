f508228df4a5d0a1bc09302e6b1b2092894270d9
Chris Antenesse <chris@antenesse.net>
2014-12-04 11:04:39 -0800

    kills hard dep on ark

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ description      'Installs Terraform (terraform.io)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends          'ark', '~> 0.6.0'
+depends          'ark'
 
 %w( amazon centos debian fedora ubuntu ).each do |os|
   supports os
