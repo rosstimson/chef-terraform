d2eeb5617e502b5141652abdc87ecac7b1dd9c5a
Chris Antenesse <chris@antenesse.net>
2014-12-07 20:32:42 -0800

    bumps version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,14 +1,14 @@
 node.default['terraform']['url_base'] = 'https://dl.bintray.com/mitchellh/terraform'
-node.default['terraform']['version'] = '0.1.0'
+node.default['terraform']['version'] = '0.3.1'
 node.default['terraform']['arch'] = kernel['machine'] =~ /x86_64/ ? 'amd64' : '386'
 
 # Transform raw output of the bintray checksum list into a Hash[filename, checksum].
 # https://dl.bintray.com/mitchellh/terraform/${VERSION}_SHA256SUMS?direct
 node.default['terraform']['raw_checksums'] = <<-EOF
-  309aed0ed61586e2682f58b77781f8e9805745a5edd1aebcddf883c9f624a0b9 0.1.0_darwin_amd64.zip
-  7925b3f1f759074c521dce21f836405f1b88347adf45233591a1d8f1a4985460 0.1.0_linux_386.zip
-  f9236171f522c9bd1c99c9f5e0ecde7f8ba881afd9f5aef8f70bdeb0a209fc22 0.1.0_linux_amd64.zip
-  ab7393f86fad7f870d398aed6984c88001b7cc36366973489cf46701dc90cf3e 0.1.0_windows_386.zip
+  dda41425c7eb06c5e8b3f5ad4904e993aa8a9ab6b61f954ee2e259667cb6ff57 0.3.1_darwin_amd64.zip
+  f773e9f75c055789e67dde80db4306d2f922cfa2f7b0d9c6413412778c594ffd 0.3.1_linux_386.zip
+  bd7b3e352a186010471818f8c50578e4afa9a2d2cf71acffb1810292db725e33 0.3.1_linux_amd64.zip
+  e5ac6a68224e2a9b4774237cf37da39722239b1a058082fb830288c20691225a 0.3.1_windows_386.zip
 EOF
 node.default['terraform']['checksums'] = Hash[
   node['terraform']['raw_checksums'].split("\n").map { |s| s.split.reverse }
