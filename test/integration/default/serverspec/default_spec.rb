67b7e71eb495b01fb64f35b1946f7b8bc021a722
Ross Timson <ross@rosstimson.com>
2014-12-08 10:22:58 +0000

    Fix Serverspec.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,8 +4,8 @@ require_relative 'spec_helper'
 
 describe 'terraform::default' do
   describe command('terraform -v') do
-    it { should return_exit_status 0 }
-    it { should return_stdout 'Terraform v0.3.1' }
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match /Terraform v0.3.1/ }
   end
 
   describe file('/usr/local/terraform-0.3.1') do
--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,8 +1,9 @@
 # Encoding: utf-8
 require 'serverspec'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
+# include Serverspec::Helper::Exec
+# include Serverspec::Helper::DetectOS
+set :backend, :exec
 
 RSpec.configure do |c|
   c.before :all do
