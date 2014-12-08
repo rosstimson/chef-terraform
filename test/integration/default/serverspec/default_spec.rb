9bf26effefe9af04a8c9e7be3df1e0d558d0da73
Chris Antenesse <chris@antenesse.net>
2014-12-07 20:51:26 -0800

    fixes integration tests given updates

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -5,20 +5,20 @@ require_relative 'spec_helper'
 describe 'terraform::default' do
   describe command('terraform -v') do
     it { should return_exit_status 0 }
-    it { should return_stdout 'Terraform v0.1.0' }
+    it { should return_stdout 'Terraform v0.3.1' }
   end
 
-  describe file('/usr/local/terraform-0.1.0') do
+  describe file('/usr/local/terraform-0.3.1') do
     it { should be_owned_by 'root' }
   end
 
   describe file('/usr/local/terraform') do
-    it { should be_linked_to '/usr/local/terraform-0.1.0' }
+    it { should be_linked_to '/usr/local/terraform-0.3.1' }
     it { should be_owned_by 'root' }
   end
 
   describe file('/usr/local/bin/terraform') do
-    it { should be_linked_to '/usr/local/terraform-0.1.0/terraform' }
+    it { should be_linked_to '/usr/local/terraform-0.3.1/terraform' }
     it { should be_owned_by 'root' }
     it { should be_executable }
   end
