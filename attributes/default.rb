5e020facc850b9d14aa7ca38833ecd2784192ae5
Dang H. Nguyen <haidangwa@gmail.com>
2015-11-24 13:50:32 -0800

    set terraform default version to 0.6.7

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['terraform']['url_base'] = 'https://dl.bintray.com/mitchellh/terraform'
-default['terraform']['version'] = '0.5.1'
+default['terraform']['version'] = '0.6.7'
 default['terraform']['arch'] = kernel['machine'] =~ /x86_64/ ? 'amd64' : '386'
 default['terraform']['zipfile'] = "terraform_#{node['terraform']['version']}_" \
   "#{node['os']}_#{node['terraform']['arch']}.zip"
@@ -8,18 +8,18 @@ default['terraform']['zipfile'] = "terraform_#{node['terraform']['version']}_" \
 # https://dl.bintray.com/mitchellh/terraform/${VERSION}_SHA256SUMS?direct
 # rubocop:disable LineLength
 default['terraform']['raw_checksums'] = <<-EOF
-  191a4150c86f9b12d922e3300a5dc884f3fcfcbf0158d3c3a9d2c82e8738037b  terraform_0.5.1_darwin_386.zip
-  5915d7668b07ea3770f1bc8126764f90723eade0245e0634af3b051ae2ceb7e5  terraform_0.5.1_darwin_amd64.zip
-  c60f23cafd309faddb592adf42a85a8b672f8d88c050a1cf8d8365c5939d0744  terraform_0.5.1_freebsd_386.zip
-  5c84d810a5e2d813ed299650cb1b3e91210a1d189a08e8c531d4cd9d5e948ed8  terraform_0.5.1_freebsd_amd64.zip
-  87e8c4d758be1f040ce902952ef7f30330c34efde798282b230e8f8ab05f0d9b  terraform_0.5.1_freebsd_arm.zip
-  97d80d595c87387205a84a4140eae6e85ca784abb7ba88b7f5cef2b7c90f377b  terraform_0.5.1_linux_386.zip
-  5f651ffd0f8d7386ed5d44d50ef0053ee974d1a73bb9becf7c99c886615a98f7  terraform_0.5.1_linux_amd64.zip
-  d28d10b1d08e8d525963dbc662ed0071f34d5133bcc8397240e5666ad38eee9c  terraform_0.5.1_linux_arm.zip
-  559562e2dd1774ea95d981acba11b8c5f47a3078aafe90e194848b9834017123  terraform_0.5.1_openbsd_386.zip
-  0e7b1b2f43411c3f950cdb82945bfa70b37e835e4c510462b720c7cac4c0ccc2  terraform_0.5.1_openbsd_amd64.zip
-  927a556566ed4c3cc9a2c64015927bf9038aa6892258451fdf9d99791a2889a5  terraform_0.5.1_windows_386.zip
-  fb0b9a1c15252c33fcc523ccf9cddaecc76ef122a129a0badea4b2817512c279  terraform_0.5.1_windows_amd64.zip
+  fb7cf062b856e0ded770fd11856113568155954aa37ec46dfa854131b9ff7d0b  terraform_0.6.7_darwin_386.zip
+  fe54fa09af11a1375a2b85912fe416d494a52137be7c5b0b4aaae35d75b0d588  terraform_0.6.7_darwin_amd64.zip
+  5862129c4922e34b1672bc0dd7b088ce6091a616171aa7ba51e0714faea8549b  terraform_0.6.7_freebsd_386.zip
+  2268c6dfca0c04618ba70e2db2da989d5adac3b7d1a3873e260ec512b20e2ee7  terraform_0.6.7_freebsd_amd64.zip
+  712a8d65869c0eaf127d89823d03d3f217c54909f62bfd5fb829e6284170e09e  terraform_0.6.7_freebsd_arm.zip
+  c12b89c42af88cebdfcfa00d8482e3c9b2ee09d419c09859423503f35def071a  terraform_0.6.7_linux_386.zip
+  2e5de08f545e117eb9825b697c5ad290ee3fdcaae7d6de4b0e99830e58b38b2e  terraform_0.6.7_linux_amd64.zip
+  e16349208eb372330aac5045739653fbbf371dc01ec16b85d064caeabbe1bb01  terraform_0.6.7_linux_arm.zip
+  55eca01309e816796e34792c8eea9c0a08f3caa3b1d5034ecf1738d37ee34f2f  terraform_0.6.7_openbsd_386.zip
+  1472437b677c08d24b1c325f69ef449bfbecd39906082a38039fada0ee4ee00c  terraform_0.6.7_openbsd_amd64.zip
+  abf7bd57f143b2de902e4230980414c48fe96e0c2fcbad0fd91aeb2c98a0dbb8  terraform_0.6.7_windows_386.zip
+  1447b66c032f71fc71014342f37e839a991ba796cfde77e74f62903bfef225c1  terraform_0.6.7_windows_amd64.zip
 EOF
 # rubocop:enable LineLength
 
