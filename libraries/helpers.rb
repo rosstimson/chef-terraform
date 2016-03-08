19a0d141a41a7346c9600d6f76e30c2fb1e70196
michael j talarczyk <mjt@mijit.com>
2016-03-08 08:24:33 -0800

    Remove extraneous slashes that give 403 Forbidden
    URLs like https://releases.hashicorp.com//terraform/0.6.11/terraform_0.6.11_SHA256SUMS now give 403 Forbidden from Hashicorp.

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -16,12 +16,11 @@ module Terraform
     # Downloads the raw checksums from hashicorp
     # https://releases.hashicorp.com/terraform/#{version}/#{checksum_file}
     def fetch_checksums
-      require 'uri'
       uri = URI.parse(node['terraform']['url_base'])
-      terraform_releases = "#{uri.scheme}://#{uri.host}/"
+      terraform_releases = "#{uri.scheme}://#{uri.host}"
       version = node['terraform']['version']
       checksum_file = "terraform_#{version}_SHA256SUMS"
-      path = "#{uri.path}/#{version}/#{checksum_file}"
+      path = "terraform/#{version}/#{checksum_file}"
       Chef::HTTP::Simple.new(terraform_releases).get(path)
     end
 
