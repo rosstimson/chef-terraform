91f8c5f7ea52529f19670df76ada328cd042f482
michael j talarczyk <mjt@mijit.com>
2016-03-09 09:33:12 -0800

    Cleaner URI expression

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -16,12 +16,9 @@ module Terraform
     # Downloads the raw checksums from hashicorp
     # https://releases.hashicorp.com/terraform/#{version}/#{checksum_file}
     def fetch_checksums
-      uri = URI.parse(node['terraform']['url_base'])
-      terraform_releases = "#{uri.scheme}://#{uri.host}"
       version = node['terraform']['version']
-      checksum_file = "terraform_#{version}_SHA256SUMS"
-      path = "terraform/#{version}/#{checksum_file}"
-      Chef::HTTP::Simple.new(terraform_releases).get(path)
+      uri = URI.join(node['terraform']['url_base'], "#{version}/terraform_#{version}_SHA256SUMS")
+      Chef::HTTP::Simple.new("#{uri.scheme}://#{uri.host}").get(uri.path.to_s)
     end
 
     def terraform_url
