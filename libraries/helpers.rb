d783c9ded87e8ace2e60ecb3789d6edfc7473d79
Dang H. Nguyen <haidangwa@gmail.com>
2016-12-28 23:30:25 -0800

    use the remote_file resource to manage the checksums file and signature file

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -1,11 +1,23 @@
 # These are helper methods that can be used in this cookbook
 # the Terraform namespace
+require 'tmpdir'
+
 module Terraform
   # Helpers belonging to the Terraform namespace
   module Helpers
+    @base = nil
+    @version = nil
+
     # Transform raw output of the checksum list into a Hash[filename, checksum].
     def raw_checksums_to_hash
-      raw_checksums = fetch_checksums
+      @base ||= URI.parse(node['terraform']['url_base'])
+      @version ||= node['terraform']['version']
+      raw_checksums = File.open(
+        File.join(Dir.tmpdir, checksums_file),
+        'r'
+      ) do |file|
+        file.read
+      end
       Hash[
         raw_checksums.split("\n").map do |s|
           s.split.reverse
@@ -13,25 +25,56 @@ module Terraform
       ]
     end
 
-    # Downloads the raw checksums from hashicorp
-    # https://releases.hashicorp.com/terraform/#{version}/#{checksum_file}
-    def fetch_checksums
-      version = node['terraform']['version']
-      base = URI.parse(node['terraform']['url_base'])
-      path = "#{base.path}/#{version}/terraform_#{version}_SHA256SUMS"
-      uri = URI.join(node['terraform']['url_base'], path)
-      Chef::HTTP::Simple.new("#{uri.scheme}://#{uri.host}").get(uri.path.to_s)
+    def sigfile
+      "#{checksums_file}.sig"
     end
 
-   
+    def checksums_file
+      "terraform_#{node['terraform']['version']}_SHA256SUMS"
+    end
+
+    # import the Hashicorp GPG key
+    def import_gpg_key
+      return if key_imported?
+      keyfile = File.join(Dir.tmpdir, 'hashicorp.asc')
+      GPGME::Key.import(File.open(keyfile))
+    end
+
+    def key_imported?
+      !GPGME::Key.find(:public, 'security@hashicorp.com', :sign).empty?
+    end
+
+    # verify the sha256sum file's signature
+    # @return: Boolean
+    def sig_verified?
+      @version ||= node['terraform']['version']
+      checksums = File.open(File.join(Dir.tmpdir, checksums_file), 'r')
+      verified = false
+      crypto = GPGME::Crypto.new
+      signature = GPGME::Data.new(
+        File.open(
+          File.join(Dir.tmpdir, "terraform_#{@version}_SHA256SUMS.sig"),
+          'rb'
+        )
+      )
+      crypto.verify(signature, signed_text: checksums) do |sig|
+        verified = sig.valid? &&
+                   !(sig.expired_signature? ||
+                   sig.expired_key? ||
+                   sig.revoked_key? ||
+                   sig.bad? ||
+                   sig.no_key?
+                    )
+      end
+      verified
+    end
+
     # See https://coderanger.net/derived-attributes/
     # for why this is the way it is
     def terraform_url
       "#{node['terraform']['url_base']}/#{node['terraform']['version']}/" \
-        "#{node['terraform']['zipfile']}" % {version: node['terraform']['version']}
+        "#{node['terraform']['zipfile']}" % { version: node['terraform']['version'] }
     end
-
-    private :fetch_checksums
   end
 end
 
