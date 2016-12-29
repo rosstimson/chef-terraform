52d9fa4f8e8207a8b8344fa3b7f27e0c35bc1f84
Dang H. Nguyen <haidangwa@gmail.com>
2016-12-28 23:49:01 -0800

    added gpgme_test.rb to run during minitest

---
+++ files/default/tests/minitest/gpgme_test.rb
@@ -0,0 +1,17 @@
+require_relative 'support/test_helper'
+
+describe_recipe 'terraform::gpgme' do
+  include ChefTerraform::TestHelper
+
+  it 'imported Hashicorp public GPG key' do
+    assert key_imported?
+  end
+
+  it 'checksums signature is accepted' do
+    assert sig_verified?
+  end
+
+  it 'installed gpgme gem' do
+    gem_package('gpgme').must_be_installed
+  end
+end
