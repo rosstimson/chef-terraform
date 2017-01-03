5084a84f4f426da8e191fe85c5f5800bf453059b
Dang H. Nguyen <haidangwa@gmail.com>
2017-01-03 14:07:01 -0800

    updated README file; made the base and version variables accessible to the recipes, added methods to DRY a few lines

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -5,19 +5,17 @@ require 'tmpdir'
 module Terraform
   # Helpers belonging to the Terraform namespace
   module Helpers
-    @base = nil
-    @version = nil
+    attr_accessor :base
+    attr_accessor :version
+
+    def initialize
+      @base = URI.parse(node['terraform']['url_base'])
+      @version = node['terraform']['version']
+    end
 
     # Transform raw output of the checksum list into a Hash[filename, checksum].
     def raw_checksums_to_hash
-      @base ||= URI.parse(node['terraform']['url_base'])
-      @version ||= node['terraform']['version']
-      raw_checksums = File.open(
-        File.join(Dir.tmpdir, checksums_file),
-        'r'
-      ) do |file|
-        file.read
-      end
+      raw_checksums = File.open(checksums_file_path, 'r').read
       Hash[
         raw_checksums.split("\n").map do |s|
           s.split.reverse
@@ -31,6 +29,14 @@ module Terraform
 
     def checksums_file
       "terraform_#{node['terraform']['version']}_SHA256SUMS"
+    end
+
+    def sigfile_path
+      File.join(Dir.tmpdir, sigfile)
+    end
+
+    def checksums_file_path
+      File.join(Dir.tmpdir, checksums_file)
     end
 
     # import the Hashicorp GPG key
@@ -47,33 +53,22 @@ module Terraform
     # verify the sha256sum file's signature
     # @return: Boolean
     def sig_verified?
-      @version ||= node['terraform']['version']
-      checksums = File.open(File.join(Dir.tmpdir, checksums_file), 'r')
-      verified = false
+      checksums = File.open(checksums_file_path, 'r')
+      io = File.open(sigfile_path, 'rb')
       crypto = GPGME::Crypto.new
-      signature = GPGME::Data.new(
-        File.open(
-          File.join(Dir.tmpdir, "terraform_#{@version}_SHA256SUMS.sig"),
-          'rb'
-        )
-      )
+      signature = GPGME::Data.new(io)
       crypto.verify(signature, signed_text: checksums) do |sig|
-        verified = sig.valid? &&
-                   !(sig.expired_signature? ||
-                   sig.expired_key? ||
-                   sig.revoked_key? ||
-                   sig.bad? ||
-                   sig.no_key?
-                    )
+        return sig.valid? &&
+               !(sig.expired_signature? || sig.expired_key? ||
+                 sig.revoked_key? || sig.bad? || sig.no_key?)
       end
-      verified
+      false
     end
 
     # See https://coderanger.net/derived-attributes/
     # for why this is the way it is
     def terraform_url
-      "#{node['terraform']['url_base']}/#{node['terraform']['version']}/" \
-        "#{node['terraform']['zipfile']}" % { version: node['terraform']['version'] }
+      "#{@base}/#{@version}/#{node['terraform']['zipfile']}"
     end
   end
 end
