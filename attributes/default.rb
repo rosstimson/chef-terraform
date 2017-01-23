e3daadc2fc7c27d1381473d6905c37d924642cda
Dang H. Nguyen <haidangwa@gmail.com>
2017-01-22 22:27:04 -0500

    fixed rubocop violation: alignment of conditionals

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,11 @@
 default['terraform']['url_base'] = 'https://releases.hashicorp.com/terraform'
 default['terraform']['version'] = '0.8.2'
-default['terraform']['arch'] = kernel['machine'] =~ /x86_64/ ? 'amd64' : '386'
+default['terraform']['arch'] = if node['kernel']['machine'] =~ /x86_64/
+                                 'amd64'
+                               else
+                                 '386'
+                               end
+
 default['terraform']['zipfile'] = "terraform_#{node['terraform']['version']}_" \
   "#{node['os']}_#{node['terraform']['arch']}.zip"
 
