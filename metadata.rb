f8751fff99c37fafc4ce84f0deb8e1945419452b
Dang H. Nguyen <haidangwa@gmail.com>
2018-05-23 10:12:53 -0700

    Fixed #28 - bumped ark dependency to ~> 3.1

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@
 name             'terraform'
 maintainer       'Ross Timson'
 maintainer_email 'ross@rosstimson.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs Terraform (terraform.io)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.0.1'
 
-depends 'ark', '~> 2.0'
+depends 'ark', '~> 3.1'
 depends 'build-essential', '~> 8.0'
 
 supports 'centos', '> 6.0'
